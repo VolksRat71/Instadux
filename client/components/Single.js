dd1ddb6a74e968f2661d075377977a32e95fe7cd
VolksRat71 <nathanieljryan1994@gmail.com>
2021-01-13 20:01:04 -0600

    Comments component is working as expected

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router'
 import Photo from './Photo';
+import Comments from './Comments'
 
 const Single = React.createClass({
     render() {
+        const i = this.props.posts.findIndex(post => post.code === this.props.params.postId);
+        const post = this.props.posts[i]
         return (
             <div className="single-photo">
-                SIngle Post
+                <Photo
+                    i={i}
+                    post={post}
+                    {...this.props}
+                />
+                <Comments />
             </div>
         )
     }
