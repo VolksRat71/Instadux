cddba6dcc681b575c94a2a91100ddf64ff16af7d
VolksRat71 <nathanieljryan1994@gmail.com>
2021-01-15 15:02:53 -0600

    Updating repo before deploying

--- client/data/comments.js
+++ client/data/comments.js
@@ -1,7 +1,7 @@
 const comments = {
   "BAhvZrRwcfu": [
     {
-      "text": "Totally need to try this.",
+      "text": "Little guy needs a nap.",
       "user": "heavymetaladam"
     }
   ],
@@ -21,50 +21,26 @@ const comments = {
   ],
   "BAPIPRjQce9": [
     {
-      "text": "Those are cute! They're like silver dollar pancakes.",
-      "user": "rrsimonsen"
+      "text": "Ooo! I prefer CAF!",
+      "user": "pThomas"
     },
     {
-      "text": "I like baby pancakes but gluten free please!! I'll bring the coffee!! See you in 6 days!!!!!! 😝😛😝♥️",
-      "user": "terzisn"
+      "text": "😝😛😝",
+      "user": "optimusprimecherry"
     },
     {
-      "text": "... and apparently growing baby. 👀. Yum.",
-      "user": "henrihelvetica"
+      "text": "👀 Yum.",
+      "user": "pumpkinsleopardnix"
     },
     {
-      "text": "@wesbos 👍 my daughter is a pancake eating machine!",
-      "user": "brentoage"
-    },
-    {
-      "text": "Nice stove!",
-      "user": "haaps"
-    },
-    {
-      "text": "Genius bottle use! Do you make a single batch of batter or make a lot and freeze it?",
-      "user": "gobananna"
-    },
-    {
-      "text": "@gobananna I just made a batch and put in in a FIFO bottle. Keeps in the fridge for a few days.",
-      "user": "wesbos"
-    },
-    {
-      "text": "@haaps thanks! It's a Jenn air - so nice to cool with!",
-      "user": "wesbos"
-    },
-    {
-      "text": "Where would you go and for how long, if you had location freedom? - Greg 🌎",
-      "user": "world_greg"
+      "text": "@volksrat 👍 my daughter is a coffee machine!",
+      "user": "rancowvirgopersona"
     }
   ],
   "BAF_KY4wcRY": [
     {
-      "text": "Looking great Wes! I'd like to see the other side of the room too.",
+      "text": "Looking great N8! I'd like to see the other side of the room too.",
       "user": "axcdnt"
-    },
-    {
-      "text": "I've never caught your podcast. Have one right? Btw - they don't have a Canary pillow? 😝",
-      "user": "henrihelvetica"
     },
     {
       "text": "Great way to start the year.",
@@ -72,24 +48,8 @@ const comments = {
     },
     {
       "text": "Are there 4k monitors?",
-      "user": "alexbaumgertner"
+      "user": "tongueabell1835"
     },
-    {
-      "text": "@axcdnt that is where I put all the junk. I'll have to clean that side too @henrihelvetica no podcast yet! @pmgllc ohh yeah! @alexbaumgertner yep - the main one is 4K - I'm loving it",
-      "user": "wesbos"
-    },
-    {
-      "text": "That chrome pillow. 😉",
-      "user": "imagesofthisandthat"
-    },
-    {
-      "text": "@wesbos is that the Dell 4k? The MacBook Pro powers it well? I also have a Retina™ / x1 setup as well. Very handy.",
-      "user": "henrihelvetica"
-    },
-    {
-      "text": "#minimalsetups",
-      "user": "wesbos"
-    }
   ],
   "_4jHytwcUA": [
     {
@@ -99,46 +59,21 @@ const comments = {
   ],
   "_zbaOlQcbn": [
     {
-      "text": "Did she get to eat her letter?",
-      "user": "pathiebert"
+      "text": "Who is he planning on pranking!?",
+      "user": "lavaplanetcellojuice"
     },
-    {
-      "text": "Nope @pathiebert! She has too much teeth now (8) so that would definitely be her first cavity 😉",
-      "user": "kaitbos"
-    }
   ],
   "_rmvQfQce8": [
     {
       "text": "A+",
-      "user": "mrjoedee"
-    },
-    {
-      "text": "I recently went to a ramen place in Philly. So amazing!",
-      "user": "jrtashjian"
+      "user": "alligatorgreenpepper"
     }
   ],
   "_ep9kiQcVy": [
     {
-      "text": "All bundled up!  Where ya goin?",
-      "user": "sophie_and_sadie"
+      "text": "When are you leavin'?",
+      "user": "intothewildflyflood"
     }
-  ],
-  "_XpJcrwcSn": [
-    {
-      "text": "Super congrats! That's wicked cool and looks great.",
-      "user": "pmgllc"
-    },
-    {
-      "text": "real live paper magazine? woah haha. flex box is awesome though, could rage quit without it",
-      "user": "tjholowaychuk2"
-    },
-    {
-      "text": "@tjholowaychuk2 haha yes! Old school stylez!",
-      "user": "wesbos"
-    }
-  ],
-  "_KnU7MwceA": [
-
   ],
   "_HMejJQcY5": [
     {
